6928d3fde46c37089ccc88ce5fd17e5a4d9db133
Anshul <anshulusr@gmail.com>
2023-09-01 12:11:06 +0530

    added source files

---
+++ main.asm
@@ -0,0 +1,8 @@
+.intel_syntax
+.text
+.globl func
+.type func, @function
+func:
+    movd %eax, 1234
+    ret
+
---
+++ main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int func();
+
+int main() {
+	printf("Should print 1234: %d\n", func());
+}
